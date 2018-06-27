09998286e0ba1d425c416fa32ac6ea95bf632a84
rotonzhang <ortonzhang@163.com>
2018-06-27 23:42:09 +0800

    test

---
+++ src/page/hello/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.box
+  h1 {{ msg }}
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.box{
+  h1{
+      font-size: 32px;
+      color: lightcoral;
+  }
+}
+</style>
